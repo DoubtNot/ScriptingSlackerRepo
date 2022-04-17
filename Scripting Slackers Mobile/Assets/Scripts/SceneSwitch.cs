001dcae2c39f1b684b2aef93028e054d5f7c9ab0
KillerFirefly <killerfirefly@gmail.com>
2022-04-17 15:53:28 -0600

    Changed lvlPanel to prefab.  Added level value to SceneSwitch.cs.  Improved Map scene.

--- Scripting Slackers Mobile/Assets/Scripts/SceneSwitch.cs
+++ Scripting Slackers Mobile/Assets/Scripts/SceneSwitch.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour {
+
+	public string level;
+
 	public void LoadScene(string name) {
 		SceneManager.LoadScene(name);
 	}
 	public void ReloadScene() {
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+	public void SetLevel(string name) {
+		level = name;
+	}
+	public void LoadLevel() {
+		SceneManager.LoadScene(level);
 	}
 }
