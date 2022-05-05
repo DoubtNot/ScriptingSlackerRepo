dc6fe2fe244df50e66300e56bf802d2d057c9c31
DoubtNot <lastof6.boyz@gmail.com>
2022-05-04 21:31:36 -0600

    Boss Race Adjusted

---
+++ Scripting Slackers Mobile/Assets/Scripts/Utilities/BossRaceDEDTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossRaceDEDTrigger : MonoBehaviour {
+
+	[SerializeField] string[] scenes;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "Boss")
+		{
+			LoadRandomLevel();
+		}
+		
+	}
+
+	public void LoadRandomLevel() {
+		if (scenes.Length > 0) {
+			int index = UnityEngine.Random.Range(0, scenes.Length);
+			SceneManager.LoadScene(scenes[index]);
+		}
+	}
+}
